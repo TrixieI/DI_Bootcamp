028d7dcc642061242e9f93402c799e37800b056a
TrixieI <fima1fim@hotmail.com>
2021-12-20 09:51:42 +0200

    better daily challenge

--- Week3/Day1/DailyChallenge/script.js
+++ Week3/Day1/DailyChallenge/script.js
@@ -9,20 +9,23 @@ let solarSystem = [
   "Neptune",
 ];
 
+let colors = [
+  "brown",
+  "yellow",
+  "green",
+  "red",
+  "gray",
+  "orange",
+  "cyan",
+  "blue",
+];
+
 let section = document.body.children[0];
 
 for (let i = 0; i < solarSystem.length; i++) {
   let planets = document.createElement("div");
   planets.className = "planet ";
   planets.className += solarSystem[i];
+  planets.style.backgroundColor = colors[i];
   section.appendChild(planets);
 }
-
-section.children[0].style.backgroundColor = "brown";
-section.children[1].style.backgroundColor = "yellow";
-section.children[2].style.backgroundColor = "green";
-section.children[3].style.backgroundColor = "red";
-section.children[4].style.backgroundColor = "gray";
-section.children[5].style.backgroundColor = "orange";
-section.children[6].style.backgroundColor = "cyan";
-section.children[7].style.backgroundColor = "blue";
