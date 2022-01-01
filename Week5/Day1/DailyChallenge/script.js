e91c1f01b6f80cc47bb573f38ec843b9d5a94513
TrixieI <fima1fim@hotmail.com>
2022-01-01 20:15:22 +0200

    DailyChallenge

---
+++ Week5/Day1/DailyChallenge/script.js
@@ -0,0 +1,22 @@
+// gatherData function will find all the input fields and create an empty object
+function gatherData() {
+  let fname = document.querySelector("#fname");
+  let lname = document.querySelector("#lname");
+  let submit = document.querySelector("#submit");
+  let object = {};
+  submit.addEventListener("click", send);
+  // the send function will be called when we click on the send button
+  // it will push our input field values into the previously created object
+  // we console.log object to make sure everything works and that our new keys and values are there
+  // we then create a new variable and use the JSON.stringify method on our object
+  // this turns our object that contains the name and last name into a JSON object
+  function send(e) {
+    e.preventDefault();
+    object.name = fname.value;
+    object.lastname = lname.value;
+    console.log(object);
+    let jsonObject = JSON.stringify(object);
+    console.log(jsonObject);
+  }
+}
+gatherData();
