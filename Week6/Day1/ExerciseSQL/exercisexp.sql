40288f4c80b10f4714a6ea6b4d5b383337704a0d
TrixieI <fima1fim@hotmail.com>
2022-01-09 11:55:10 +0200

    exerciseXP SQL

---
+++ Week6/Day1/ExerciseSQL/exercisexp.sql
@@ -0,0 +1,44 @@
+-- CREATE TABLE items (
+-- item_id SERIAL PRIMARY KEY,
+-- item_name VARCHAR (20) NOT NULL,
+-- item_price SMALLINT NOT NULL
+-- )
+
+-- CREATE TABLE customers (
+-- customer_id SERIAL PRIMARY KEY,
+-- firstName VARCHAR (20) NOT NULL,
+-- lastName VARCHAR (25) NOT NULL
+-- )
+
+-- INSERT INTO items (item_name, item_price)
+-- VALUES('Small Desk', 100)
+-- VALUES('Large Desk', 300)
+-- VALUES('Fan', 80)
+
+-- INSERT INTO customers(firstName, lastName)
+-- VALUES('Greg', 'Jones')
+
+-- INSERT INTO customers(firstName, lastName)
+-- VALUES('Sandra', 'Jones')
+
+-- INSERT INTO customers(firstName, lastName)
+-- VALUES('Scott', 'Scott')
+
+-- INSERT INTO customers(firstName, lastName)
+-- VALUES('Trevor', 'Green')
+
+-- INSERT INTO customers(firstName, lastName)
+-- VALUES('Melanie', 'Johnson')
+
+-- SELECT * FROM items
+
+-- SELECT * FROM items WHERE item_price>80
+
+-- SELECT * FROM items WHERE item_price<=300
+
+-- SELECT * FROM customers WHERE lastName = 'Smith'
+
+-- SELECT * FROM customers WHERE lastName = 'Jones'
+
+-- SELECT * FROM customers WHERE firstName != 'Scott'
+
