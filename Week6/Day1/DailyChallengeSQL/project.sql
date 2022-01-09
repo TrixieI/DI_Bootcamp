f8f4fd2f5e3e3a5423597d6cfb945fca342ea65f
TrixieI <fima1fim@hotmail.com>
2022-01-09 13:27:17 +0200

    added project

---
+++ Week6/Day1/DailyChallengeSQL/project.sql
@@ -0,0 +1,15 @@
+-- event_datetime,city,temperature,light,airquality_raw,sound,humidity,dust
+-- 2015-02-27T21:00:00.000Z,San
+
+-- CREATE TABLE cityinfo(
+-- event_datetime TIMESTAMP NOT NULL,
+-- city VARCHAR (30) NOT NULL,
+-- temperature DECIMAL NOT NULL,
+-- light DECIMAL NOT NULL,
+-- airquality_raw DECIMAL NOT NULL,
+-- sound DECIMAL NOT NULL,
+-- humidity DECIMAL NOT NULL,
+-- dust DECIMAL NOT NULL
+-- )
+
+-- SELECT * FROM cityinfo
