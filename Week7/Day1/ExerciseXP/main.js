061aceb298e87b383dcc75d1b3641ca37d342137
TrixieI <fima1fim@hotmail.com>
2022-01-16 12:45:14 +0200

    ExerciseXP

---
+++ Week7/Day1/ExerciseXP/main.js
@@ -0,0 +1,23 @@
+// Exercise 1
+
+let a = 5;
+let b = 10;
+let age = a + b;
+
+console.log(`Hello you are ${age}`);
+
+// Exercise 2
+
+console.log("Hi! thanks for running me with npm start!");
+
+// Exercise 3
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b; // x = 15
+
+console.log(`Exercise 3, log a+b = ${x}
+
+updated, sum of x+c = ${x + c}`);
